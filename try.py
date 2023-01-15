95a65f259846acda40d8b757b15163bd42465364
scalvaruso <calvaruso.simone@gmail.com>
2023-01-15 02:09:10 +0000

    temp file

---
+++ try.py
@@ -0,0 +1,12 @@
+from border import border
+
+menu_list = ["\t\t\t\ttry title",
+    "",
+    "try line 1",
+    "try line 2",
+    "try line 3",
+    "try line 4",
+    "try line 5",
+    ]
+
+print(border(menu_list))
